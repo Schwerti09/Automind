5dd8b100c6f7b26e21124e1c6fced0d95ccaf33d
Schwerti09 <rps-vertrieb@t-online.de>
2025-06-25 00:30:38 +0200

    Delete verify (1).ts

--- verify (1).ts
+++
@@ -1,6 +0,0 @@
-// services/verify.ts
-
-export function verifyToken(token: string): boolean {
-  // Platzhalter-Logik – hier später JWT oder API-Token-Check einbauen
-  return token === "automind-master-token";
-}
